4b68100519bf1a4ad80cfb829ca596cbbdb09493
KeitoAV <keito_ekka@mail.ru>
2022-12-14 18:01:16 +0300

    /core/signup

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from core import views
 from core.views import SignUpView
 
 urlpatterns = [
 
     path('', views.index),
-
-    path('signup/', SignUpView.as_view(), name='signup'),
+    path('signup', SignUpView.as_view(), name='signup'),
 ]
