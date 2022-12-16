58fa4f8e84d8c93c8c0774a570a73167f7398168
KeitoAV <keito_ekka@mail.ru>
2022-12-16 10:32:47 +0300

    OAuth

--- core/serializers.py
+++ core/serializers.py
@@ -3,7 +3,6 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework.exceptions import AuthenticationFailed
-
 from core.models import User
 
 
